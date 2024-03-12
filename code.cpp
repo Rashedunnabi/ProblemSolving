2c3c58f56c66bea2ad2dc8661da6e80a7e5478c1
Rashedunnabi <mramuhit@gmail.com>
2024-03-12 19:48:33 +0600

    Add e new problem

--- code.cpp
+++ code.cpp
@@ -1,51 +1,61 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define Faster ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define ll long long
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-
-const int mod = 1e9 + 7;
-const int N = 2e5 + 7;
-
-void solution()
-{
-    int n, i;
-    cin >> n;
-    string s;
-    cin >> s;
-
-    int p = 0, m = 0, mp = 0, ans = 0;
-
-    for (i = 0; i < n - 2; i++)
-    {
-        string ss = s.substr(i, 3);
-        if (ss == "pie")
-            p++;
-        if (ss == "map")
-            m++;
-    }
-    for (i = 0; i < n - 4; i++)
-    {
-        string ss = s.substr(i, 5);
-        if (ss == "mapie")
-            mp++;
-    }
-    ans = p + m - mp;
-    cout << ans << '\n';
-}
+#define _test     \
+    int _TEST;    \
+    cin >> _TEST; \
+    while (_TEST--)
+#define ff first
+#define ss second
+#define pb push_back
 
 int main()
 {
-    Faster;
-    int t = 1;
-    cin >> t;
-    while (t--)
+    _test
     {
-        solution();
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<int> a(n), b(m), c(k);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        for (int i = 0; i < m; i++)
+            cin >> b[i];
+        for (int i = 0; i < k; i++)
+            cin >> c[i];
+
+        vector<array<int, 2>> d(n - 1);
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            d[i][0] = a[i + 1] - a[i];
+            d[i][1] = i;
+        }
+
+        sort(d.rbegin(), d.rend());
+        sort(b.begin(), b.end());
+        sort(c.begin(), c.end());
+
+        int res1 = d[0][0], res2 = 0;
+        int id = d[0][1];
+        if (n > 2)
+            res2 = d[1][0];
+
+        int res = res1;
+        for (int i = 0; i < m; i++)
+        {
+            int l = a[id], r = a[id + 1];
+            int mid = ((ll)l + r) / 2;
+            int t = lower_bound(c.begin(), c.end(), mid - b[i]) - c.begin();
+            for (int j = max(0, t - 3); j <= min(k - 1, t + 3); j++)
+            {
+                if (b[i] + c[j] >= l && b[i] + c[j] <= r)
+                    res = min(res, max(b[i] + c[j] - a[id], a[id + 1] - b[i] - c[j]));
+            }
+        }
+
+        cout << max(res, res2) << '\n';
     }
-    return 0;
 }
