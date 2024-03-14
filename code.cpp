bc1e45949e149d76032d4795aa6bce54dcdec0b5
Rashedunnabi <mramuhit@gmail.com>
2024-03-14 17:14:11 +0600

    1800+ rating problems solve on codeforces

--- code.cpp
+++ code.cpp
@@ -1,61 +1,63 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define ll long long
-#define _test     \
-    int _TEST;    \
-    cin >> _TEST; \
-    while (_TEST--)
-#define ff first
-#define ss second
-#define pb push_back
+
+void solution()
+{
+    ll n, m, k, d, i;
+    cin >> n >> m >> k >> d;
+
+    vector<ll> ans(n + 1);
+
+    for (ll i = 0; i < n; i++)
+    {
+        vector<ll> v(m);
+        for (ll &i : v)
+            cin >> i;
+
+        multiset<ll> mst;
+        deque<ll> q;
+
+        mst.insert(1);
+        q.push_back(1);
+
+        for (ll j = 1; j < m; j++)
+        {
+            ll cost = *mst.begin() + v[j] + 1LL;
+            mst.insert(cost);
+            q.push_back(cost);
+
+            if (q.size() > d + 1)
+            {
+                auto it = mst.find(q.front());
+                mst.erase(it);
+                q.pop_front();
+            }
+        }
+        ans[i + 1] = q.back();
+    }
+
+    for (i = 1; i <= n; i++)
+        ans[i] += ans[i - 1];
+
+    ll mini = LONG_LONG_MAX;
+
+    for (i = 0; i <= n - k; i++)
+    {
+        mini = min(mini, ans[i + k] - ans[i]);
+    }
+    cout << mini << '\n';
+}
 
 int main()
 {
-    _test
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
     {
-        int n, m, k;
-        cin >> n >> m >> k;
-        vector<int> a(n), b(m), c(k);
-
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
-        for (int i = 0; i < m; i++)
-            cin >> b[i];
-        for (int i = 0; i < k; i++)
-            cin >> c[i];
-
-        vector<array<int, 2>> d(n - 1);
-
-        for (int i = 0; i < n - 1; i++)
-        {
-            d[i][0] = a[i + 1] - a[i];
-            d[i][1] = i;
-        }
-
-        sort(d.rbegin(), d.rend());
-        sort(b.begin(), b.end());
-        sort(c.begin(), c.end());
-
-        int res1 = d[0][0], res2 = 0;
-        int id = d[0][1];
-        if (n > 2)
-            res2 = d[1][0];
-
-        int res = res1;
-        for (int i = 0; i < m; i++)
-        {
-            int l = a[id], r = a[id + 1];
-            int mid = ((ll)l + r) / 2;
-            int t = lower_bound(c.begin(), c.end(), mid - b[i]) - c.begin();
-            for (int j = max(0, t - 3); j <= min(k - 1, t + 3); j++)
-            {
-                if (b[i] + c[j] >= l && b[i] + c[j] <= r)
-                    res = min(res, max(b[i] + c[j] - a[id], a[id + 1] - b[i] - c[j]));
-            }
-        }
-
-        cout << max(res, res2) << '\n';
+        solution();
     }
+    return 0;
 }
