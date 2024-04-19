906596d0372ebf0602708317f2a3684dfe86c968
Rashedunnabi <mramuhit@gmail.com>
2024-04-19 08:10:24 +0600

    Cf Educational 164

--- CodeForces/A_Painting_the_Ribbon.cpp
+++ CodeForces/A_Painting_the_Ribbon.cpp
@@ -1,61 +1,24 @@
-// 9876543210...???
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
-#define endl "\n"
-ll n, x, i, p, q, ans, y, l, r, qr, m, a, b, c, j, k;
-string s;
-
-long long power(long long A, long long B)
-{
-    if (B == 0)
-        return 1;
-    long long ress = power(A, B / 2);
-    if (B % 2)
-        return ress * ress * A;
-    else
-        return ress * ress;
-}
-
-void primeFactors(ll ww)
-{
-    while (ww % 2 == 0)
-    {
-        ww = ww / 2;
-    }
-    for (ll ii = 3; ii <= sqrt(ww); ii = ii + 2)
-    {
-        while (ww % ii == 0)
-        {
-            ww = ww / ii;
-        }
-    }
-}
-
-void solve()
-{
-    ll n, m, k;
-    cin >> n >> m >> k;
-
-    ll value = n / m, rem = n % m;
-    ll ans = (m - 1) * value + max(rem - 1, 0LL);
-    if (ans > k)
-        yes;
-    else
-        no;
-}
-
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 int main()
 {
-
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
-    ll t = 1;
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t = 1;
     cin >> t;
     while (t--)
     {
-        solve();
+        int n, m, k, val;
+        cin >> n >> m >> k;
+
+        val = (n + m - 1) / m;
+        val = n - val;
+        if (val > k)
+            yes;
+        else
+            no;
     }
     return 0;
 }
--- CodeForces/C_Long_Multiplication.cpp
+++ CodeForces/C_Long_Multiplication.cpp
@@ -1,65 +1,37 @@
-// 9876543210...???
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define yes cout << "YES" << endl;
-#define no cout << "NO" << endl;
-#define endl "\n"
-
-long long power(long long A, long long B)
-{
-    if (B == 0)
-        return 1;
-    long long ress = power(A, B / 2);
-    if (B % 2)
-        return ress * ress * A;
-    else
-        return ress * ress;
-}
-
-void primeFactors(ll ww)
-{
-    while (ww % 2 == 0)
-    {
-        ww = ww / 2;
-    }
-    for (ll ii = 3; ii <= sqrt(ww); ii = ii + 2)
-    {
-        while (ww % ii == 0)
-        {
-            ww = ww / ii;
-        }
-    }
-}
-
-void solve()
-{
-    string str1, str2;
-    cin >> str1 >> str2;
-    if (str1 < str2)
-        swap(str1, str2);
-
-    bool flag = 0;
-    for (int i = 0; i < str1.size(); i++)
-    {
-        if (str1[i] > str2[i] && flag)
-            swap(str1[i], str2[i]);
-        if (str1[i] != str2[i])
-            flag = 1;
-    }
-    cout << str1 << '\n'
-         << str2 << endl;
-}
 
 int main()
 {
-
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
-    ll t = 1;
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t = 1;
     cin >> t;
     while (t--)
     {
-        solve();
+        string a, b;
+        cin >> a >> b;
+        if (a < b)
+            swap(a, b);
+        bool flag = 0;
+
+        int i, n = a.length();
+        for (i = 0; i < n; i++)
+        {
+            if (a[i] == b[i])
+                continue;
+            if (a[i] != b[i])
+            {
+                if (!flag)
+                    flag = 1;
+                else
+                {
+                    if (a[i] > b[i])
+                        swap(a[i], b[i]);
+                }
+            }
+        }
+        cout << a << '\n'
+             << b << '\n';
     }
     return 0;
 }
