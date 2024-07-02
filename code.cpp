96d27fb80d9f872458c09fddc39c671c3f22eff7
rashedunnabi <mramuhit@gmail.com>
2024-07-03 02:23:37 +0600

    edit code file

--- code.cpp
+++ code.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 int32_t main()
 {
     ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
@@ -8,22 +12,6 @@ int32_t main()
     cin >> t;
     while (t--)
     {
-        int n, k, ans = 0;
-        cin >> n >> k;
-
-        int numOfOdd = (n + 1) / 2;
-        for (int i = 0; i < 32; i++)
-        {
-            int numOfValue = n / (1LL << i);
-            numOfOdd = (numOfValue + 1) / 2;
-            if (k <= numOfOdd)
-            {
-                ans = (2 * k - 1) * (1LL << i);
-                break;
-            }
-            k -= numOfOdd;
         }
-        cout << ans << '\n';
-    }
     return 0;
 }
